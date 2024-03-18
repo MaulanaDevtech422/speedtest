a5a921c2852a9b5db53bc6a25a918ece3bb72e0e
MaulanaDevtech422 <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-03-18 10:34:09 +0700

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,20 @@
+#script by MaulanaDevtech
+#boleh di copy asal tidak di jadikan hak milik
+#salam technologi
+
+
+figlet maulananet.com | lolcat
+date | lolcat
+echo -e "\033[0;35mtest internet...."
+speedtest-cli
+
+#cara install via github
+
+# pkg update && pkg upgrade
+# pkg install python3 -y
+# pip install speedtest-cli
+# pkg install php
+# cd speedtest
+# chmod +x speedtest.sh
+# ./speedtest.sh
+#finish dan script bisa di jalankan
